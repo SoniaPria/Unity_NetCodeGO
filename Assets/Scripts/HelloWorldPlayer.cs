647ce1ba4871ba65d342103a9259ab74fc4a661d
SoniaPria <webusable@protonmail.com>
2023-05-09 14:08:18 +0200

    4 movementos por Input de xogadores en rede

--- Assets/Scripts/HelloWorldPlayer.cs
+++ Assets/Scripts/HelloWorldPlayer.cs
@@ -58,31 +58,60 @@ namespace HelloWorld
 			}
 		}
 
-		public void MoveToLeft()
+		public void MoveOnRequest(int key)
 		{
 			if (!IsOwner) { return; }
+
+			Vector3 direction;
+
+			Debug.Log($"{gameObject.name}.HelloWorldPlayer.MoveOnRequest({key})");
+
+			switch (key)
+			{
+				case 1:
+					direction = Vector3.forward;
+					break;
+
+				case 2:
+					direction = Vector3.right;
+					break;
+
+				case 3:
+					direction = Vector3.back;
+					break;
+
+				case 4:
+					direction = Vector3.left;
+					break;
+
+				default:
+					direction = Vector3.zero;
+
+					Debug.Log($"\t A tecla pulsada non é valida para mover");
+					break;
+			}
+
+			Debug.Log($"\t New direction: {direction}");
+
 			if (NetworkManager.Singleton.IsServer)
 			{
-				// Calculamos a posición que se pide por teclado
-				Vector3 newPosition = transform.position + Vector3.left;
 				// Actualizamos a posición na variable de rede
-				Position.Value += Vector3.left;
+				Position.Value += direction;
 
-				Debug.Log($"{gameObject.name}.HelloWorldPlayer.MoveToLeft");
-				Debug.Log($"\t new.position: {newPosition}");
+				Debug.Log($"\t new.position: {Position.Value}");
 			}
 			else
 			{
-				SubmitLeftPositionRequestServerRpc();
+				SubmitToPositionRequestServerRpc(direction);
 			}
 		}
 
 		[ServerRpc]
-		void SubmitLeftPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+		void SubmitToPositionRequestServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
 		{
-			Position.Value += Vector3.left;
+			Position.Value += direction;
 
-			Debug.Log($"{gameObject.name}.HelloWorldPlayer.SubmitLeftPositionRequestServerRpc");
+			Debug.Log($"{gameObject.name}.HelloWorldPlayer.SubmitToPositionRequestServerRpc({direction})");
 			Debug.Log($"\t new.position: {Position.Value}");
 		}
 
@@ -93,8 +122,6 @@ namespace HelloWorld
 			Position.Value = GetRandomPositionOnPlane();
 		}
 
-
-
 		static Vector3 GetRandomPositionOnPlane()
 		{
 			return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
@@ -102,20 +129,40 @@ namespace HelloWorld
 
 		void Update()
 		{
+			if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				Debug.Log($"{gameObject.name}.HelloWorldPlayer.Update");
+				Debug.Log($"\t Input W | Input Up arrow");
+
+				MoveOnRequest(1);
+			}
+
+			if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				Debug.Log($"{gameObject.name}.HelloWorldPlayer.Update");
+				Debug.Log($"\t Input D | Input Right arrow");
+
+				MoveOnRequest(2);
+			}
+
+			if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				Debug.Log($"{gameObject.name}.HelloWorldPlayer.Update");
+				Debug.Log($"\t Input S | Input Down arrow");
+
+				MoveOnRequest(3);
+			}
+
 			if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
 			{
 				Debug.Log($"{gameObject.name}.HelloWorldPlayer.Update");
-				Debug.Log($"\t Input A | Input left arrow");
+				Debug.Log($"\t Input A | Input Left arrow");
 
-				MoveToLeft();
+				MoveOnRequest(4);
 			}
-
 
 			// Asigna a posición da variable de rede ao game object
 			transform.position = Position.Value;
-
-			// Debug.Log($"{gameObject.name}.HelloWorldPlayer.Update");
-			// Debug.Log($"\t transform.position: {transform.position}");
 		}
 	}
 }
