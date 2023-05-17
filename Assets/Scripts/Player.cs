a04c567f7d508d62e05ae17fdfb1e637f88e81c9
SoniaPria <webusable@protonmail.com>
2023-05-17 13:31:05 +0200

    Elimina o namespace HelloWorld

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,84 +1,81 @@
 using Unity.Netcode;
 using UnityEngine;
 
-namespace HelloWorld
+public class Player : NetworkBehaviour
 {
-    public class Player : NetworkBehaviour
+    float jumpForce;
+
+    void Start()
     {
-        float jumpForce;
+        jumpForce = 6f;
+    }
 
-        void Start()
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
         {
-            jumpForce = 6f;
+            // Pide ao server una posición aleatoria 
+            // para que non se vexan todos no mesmo punto
+
+            MoveServerRpc(GetRandomPosition2D());
+        }
+    }
+
+    bool IsJumping() { return transform.position.y > 1.05f; }
+
+
+    Vector3 GetRandomPosition2D()
+    {
+        return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+    }
+
+
+    [ServerRpc]
+    void MoveServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
+    {
+        transform.position += direction;
+    }
+
+    [ServerRpc]
+    void JumpServerRpc(ServerRpcParams rpcParams = default)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        if (!IsOwner) { return; }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            MoveServerRpc(Vector3.forward);
         }
 
-        public override void OnNetworkSpawn()
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (IsOwner)
-            {
-                // Pide ao server una posición aleatoria 
-                // para que non se vexan todos no mesmo punto
 
-                MoveServerRpc(GetRandomPosition2D());
-            }
+            MoveServerRpc(Vector3.right);
         }
 
-        bool IsJumping() { return transform.position.y > 1.05f; }
-
-
-        Vector3 GetRandomPosition2D()
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+
+            MoveServerRpc(Vector3.back);
         }
 
-
-        [ServerRpc]
-        void MoveServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += direction;
+
+            MoveServerRpc(Vector3.left);
+
         }
 
-        [ServerRpc]
-        void JumpServerRpc(ServerRpcParams rpcParams = default)
+        if (Input.GetButtonDown("Jump") && !IsJumping())
         {
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-        }
 
-        void Update()
-        {
-            if (!IsOwner) { return; }
-
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-
-                MoveServerRpc(Vector3.forward);
-            }
-
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-
-                MoveServerRpc(Vector3.right);
-            }
-
-            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            {
-
-                MoveServerRpc(Vector3.back);
-            }
-
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-
-                MoveServerRpc(Vector3.left);
-
-            }
-
-            if (Input.GetButtonDown("Jump") && !IsJumping())
-            {
-
-                JumpServerRpc();
-            }
+            JumpServerRpc();
         }
     }
 }
